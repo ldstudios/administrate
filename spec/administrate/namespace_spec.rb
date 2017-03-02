2b1eac76cff55df4dc859e31fe089832d2941675
Daniel Rice <daniel.rice@ldstudios.co>
2017-03-02 01:40:56 -0500

    [#179] Allowing Administrate Namespace class to return ActiveRecord/Model classes.  This allows one to reference ActiveModel’s route values (route_key, singular_route_key) when generating Polymorphic URLs.

--- spec/administrate/namespace_spec.rb
+++ spec/administrate/namespace_spec.rb
@@ -16,4 +16,19 @@ describe Administrate::Namespace do
       end
     end
   end
+
+  describe "#models" do
+    it "searches the routes for ActiveRecord models in the namespace" do
+      begin
+        namespace = Administrate::Namespace.new(:admin)
+        Rails.application.routes.draw do
+          namespace(:admin) { resources :customers }
+        end
+
+        expect(namespace.models).to eq [::Customer]
+      ensure
+        reset_routes
+      end
+    end
+  end
 end
