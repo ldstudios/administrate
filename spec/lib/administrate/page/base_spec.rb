ab5fa8ee19e3fa2ace65bc67d817dd8ea7283b5d
Daniel Rice <daniel.rice@ldstudios.co>
2017-03-02 15:29:27 -0500

    Add some pending specs - I’ll get them working later.

---
+++ spec/lib/administrate/page/base_spec.rb
@@ -0,0 +1,6 @@
+require "administrate/page/base"
+
+#Need to add Test Coverage for this model as I'm editing it directly.
+RSpec.describe Administrate::Page::Base do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
